5d7427137ae0a056b68ea5b4ed5faaed5885a946
Allan Caffee <allanc@aweber.com>
2011-07-29 21:18:30 -0400

    Add base for sphinx documentation

---
+++ doc/source/conf.py
@@ -0,0 +1,34 @@
+import sys, os
+
+# If your extensions are in another directory, add it here. If the directory
+# is relative to the documentation root, use os.path.abspath to make it
+# absolute, like shown here.
+sys.path.insert(0, os.path.abspath('.'))
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.ifconfig',
+]
+
+copyright = 'AWeber Communications'
+pygments_style = 'sphinx'
+
+templates_path = ['_templates']
+exclude_trees = ['build']
+html_static_path = ['_static']
+
+html_theme = 'nature'
+
+intersphinx_mapping = {
+    'http://docs.python.org/': None,
+}
+
+master_doc = 'index'
+todo_include_todos = True
+
+source_suffix = '.rst'
+project = 'base_server'
---
+++ doc/source/index.rst
@@ -0,0 +1,11 @@
+Table of Contents
+=================
+
+.. toctree::
+
+
+Indices and tables
+------------------
+
+* :ref:`genindex`
+* :ref:`search`
