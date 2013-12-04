e82a51b3d9ba66221770d8e0e23bce906a0e2f40
John Brodie <johnb@aweber.com>
2013-12-04 11:02:18 -0500

    tests: Fix coverage
    Ignore these two lines, as one
    will never be "covered" during
    tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,9 @@
 import sys
 
 if sys.version_info >= (2, 7):
-    import unittest
+    import unittest  # pragma: no cover
 else:
-    import unittest2 as unittest
+    import unittest2 as unittest  # pragma: no cover
 
 try:
     from unittest import mock
