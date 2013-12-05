a39bdc0596d8103e880dc6b9b2774506d9af4f91
John Brodie <johnb@aweber.com>
2013-12-05 08:53:34 -0500

    tests: More import improvements

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,20 @@
+"""
+Testing
+-------
+Cross-version imports of testing tools.
+
+This module allows tests to run under all
+recent Python versions without requiring changes
+to each import in our tests.
+
+Both mock and unittest should be imported from
+this module to reap these benefits.  For example:
+
+    from tests.mock import Mock, patch
+    from tests import unittest
+
+"""
+
 import sys
 
 if sys.version_info >= (2, 7):
@@ -6,8 +23,9 @@ else:
     import unittest2 as unittest  # pragma: no cover
 
 try:
-    from unittest import mock
+    from unittest import mock  # pragma: no cover
 except ImportError:
-    import mock
+    import mock  # pragma: no cover
 
 sys.modules[__package__ + '.mock'] = mock
+sys.modules[__package__ + '.unittest'] = unittest
